5487e50f777a0d5fcfbbf8a32df64eb74b2fb10c
digidon-son <e.lepehin93@gmail.com>
2022-05-07 10:14:53 +0300

    add js and styles, edit pages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :user, presence: true
   validates :address, presence: true
   validates :datetime, presence: true
   validates :title, presence: true, length: { maximum: 255 }
