4f7226a3719b3b89b389c2a68d6ee129d855e87b
digidon-son <e.lepehin93@gmail.com>
2022-05-13 18:11:51 +0300

    changed the access method and removed the signature button on my own event

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  helper_method :current_user_can_edit?
+  helper_method :current_user_has_access?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [:password, :password_confirmation, :current_password])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 
-  def current_user_can_edit?(model)
+  def current_user_has_access?(model)
     user_signed_in? && (
       model.user == current_user ||
         (model.try(:event).present? && model.event.user == current_user)
