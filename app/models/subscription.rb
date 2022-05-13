a7499d2d1df84aa4d607aeb8ea999199a523cdfb
digidon-son <e.lepehin93@gmail.com>
2022-05-13 12:25:05 +0300

    genarate subscription controllers

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -6,6 +6,8 @@ class Subscription < ApplicationRecord
 
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
 
   def user_name
     if user.present?
