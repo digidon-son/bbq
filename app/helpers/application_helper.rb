69e346e76b08b9f0ea7de7f3e729144fd05c1cb9
digidon-son <e.lepehin93@gmail.com>
2022-05-15 12:48:21 +0300

    added thumb photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,32 @@
 module ApplicationHelper
+
+  # Возвращает адрес рандомной фотки события, если есть хотя бы одна
+  # Или ссылку на картинку по умолчанию
+  def event_photo(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.url
+    else
+      asset_path('event.jpg')
+    end
+  end
+
+  # Возвращает миниатюрную версию фотки
+  def event_thumb(event)
+    photos = event.photos.persisted
+
+    if photos.any?
+      photos.sample.photo.thumb.url
+    else
+      asset_path('event_thumb.jpg')
+    end
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', class: "fa fa-#{icon_class}"
+  end
+
   def user_avatar(user)
     if user.avatar?
       user.avatar.url
@@ -7,8 +35,11 @@ module ApplicationHelper
     end
   end
 
-  def fa_icon(icon_class)
-    content_tag 'span', class: "fa fa-#{icon_class}"
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('user.png')
+    end
   end
-
 end
