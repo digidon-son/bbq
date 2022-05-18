8cd016da44bad7572b9e1a8a9ee638820d307c90
digidon-son <e.lepehin93@gmail.com>
2022-05-18 10:13:23 +0300

    edited valides in subscribtion model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,43 +1,34 @@
+# frozen_string_literal: true
+
+# Класс подписок на событие
 class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :event, presence: true
-
   validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
+  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
 
   validate :email_in_use
 
   validate :organizer_cant_subscribe
 
   def user_name
-    if user.present?
-      user.name
-    else
-      super
-    end
+    user&.name || super
   end
 
   def user_email
-    if user.present?
-      user.email
-    else
-      super
-    end
+    user&.email || super
   end
 
   private
 
   def email_in_use
-    if User.where(email: self.user_email.downcase).first
-      errors.add(:user_email, I18n.t('controllers.subscriptions.busy'))
-    end
+    errors.add(:user_email, I18n.t('controllers.subscriptions.busy')) if User.where(email: user_email.downcase).first
   end
 
   def organizer_cant_subscribe
-    errors.add(:organizer, I18n.t('controllers.subscriptions.organizer')) if user == event.user
+    errors.add(:user, I18n.t('controllers.subscriptions.organizer')) if user == event.user
   end
 end
