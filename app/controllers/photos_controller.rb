8fb7b690dee642ffc8188620734a9fbed21ae4a5
digidon-son <e.lepehin93@gmail.com>
2022-05-20 08:54:35 +0300

    localized the header of the letter, rewrote the methods and views of the mailer

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -66,7 +66,7 @@ class PhotosController < ApplicationController
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
     all_emails.each do |mail|
-      EventMailer.photo(event, photo, mail).deliver_now
+      EventMailer.photo(photo, mail).deliver_now
     end
   end
 end
--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -4,19 +4,18 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: event.user.email, subject: default_i18n_subject(event_name: @event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
-    mail to: email, subject: "Новый коментарий @#{event.title}"
+    mail to: email, subject: default_i18n_subject(commenter: comment.user_name)
   end
 
-  def photo(event, photo, email)
-    @event = event
+  def photo(photo, email)
     @photo = photo
     @email = email
-    mail to: email, subject: "Добавлена новая фотография для @#{event.title}"
+    mail to: email, subject: default_i18n_subject(event_name: photo.event.title)
   end
 end
